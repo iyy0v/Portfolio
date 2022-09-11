112382156f546805e8672e022e3cf6c989318ab5
iyy0v <naitmihoub.ayoub@gmail.com>
2022-09-11 19:33:18 +0100

    removed all warnings

--- v3.0/scripts.js
+++ v3.0/scripts.js
@@ -2,7 +2,6 @@ let logo = document.getElementById("logo");
 let logoText = document.getElementById("logo").getElementsByTagName("button")[0].getElementsByTagName("p")[0];
 let mode = document.getElementById("switch");
 let modeText = document.getElementById("mode");
-let pfp = document.getElementById("pfp");
 let intro = document.getElementById("intro");
 let arrow = document.getElementById("scroll");
 
@@ -119,14 +118,6 @@ mode.onmouseover = function() {
 mode.onmouseleave = function() {
     document.getElementById("mode").style.color = oldColor;
 } 
-
-
-pfp.onclick = function() {
-    pfp.src = "./img/pepohi.gif";
-    setTimeout(function(){
-        pfp.src = "./img/pepohi-pre.jpg";
-    }, 2100);
-}
 
 
 window.onscroll = () => {
