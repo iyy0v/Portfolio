3b7686627fdbea53380698d24f00aa6b2d4204ae
iyy0v <naitmihoub.ayoub@gmail.com>
2025-10-28 17:16:42 +0100

    Added CV + Zimou experience

--- v4.0/app/(sections)/Contact.tsx
+++ v4.0/app/(sections)/Contact.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { ErrorMessage, Field, Form, Formik } from "formik";
@@ -6,171 +6,228 @@ import { toFormikValidationSchema } from "zod-formik-adapter";
 import { validationSchema } from "@/utils/validations";
 import sendEmail from "@/utils/email";
 import Socials from "./Socials";
-import { Button, Container, Flex, FormControl, FormLabel, Grid, GridItem, Text } from "@chakra-ui/react";
-import { useToast } from '@chakra-ui/react'
+import {
+	Button,
+	Container,
+	Flex,
+	FormControl,
+	FormLabel,
+	Grid,
+	GridItem,
+	Text,
+} from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
 
 type ContactProps = {
-    name: string,
-    email: string,
-    subject: string,
-    message: string
-}
+	name: string;
+	email: string;
+	subject: string;
+	message: string;
+};
 
 export default function Contact() {
-    const [isLoading, setLoading] = useState(false);
-    const toast = useToast()
+	const [isLoading, setLoading] = useState(false);
+	const toast = useToast();
 
-    const handleSubmit = async (
-        values: ContactProps,
-        {
-            setSubmitting,
-            resetForm,
-        } : {
-            setSubmitting: (isUbmitting: boolean) => void;
-            resetForm: () => void;
-        }
-        
-    ) => {
-        try {
-            setLoading(true)
-            await sendEmail({
-                name: values.name,
-                email: values.email,
-                subject: values.subject,
-                message: values.message
-            }).then((res) => {
-                toast({
-                    title: 'Your message was sent.',
-                    description: "Thank you!",
-                    status: 'success',
-                    variant: 'left-accent',
-                    position: 'bottom-right',
-                    duration: 3000,
-                })
-            });
+	const handleSubmit = async (
+		values: ContactProps,
+		{
+			setSubmitting,
+			resetForm,
+		}: {
+			setSubmitting: (isUbmitting: boolean) => void;
+			resetForm: () => void;
+		}
+	) => {
+		try {
+			setLoading(true);
+			await sendEmail({
+				name: values.name,
+				email: values.email,
+				subject: values.subject,
+				message: values.message,
+			}).then((res) => {
+				toast({
+					title: "Your message was sent.",
+					description: "Thank you!",
+					status: "success",
+					variant: "left-accent",
+					position: "bottom-right",
+					duration: 3000,
+				});
+			});
 
-            resetForm()
+			resetForm();
 
-            console.log("Email sent successfully!");
-        } catch (error) {
-            toast({
-                title: 'Failed to send message.',
-                description: "Please try again later, or contact me using my email..",
-                status: 'error',
-                variant: 'left-accent',
-                position: 'bottom-right',
-                duration: 3000,
-            })
-            console.error("Failed to send email: ", error);
-        } finally {
-            setSubmitting(false)
-            setLoading(false);
-        }
-    }
+			console.log("Email sent successfully!");
+		} catch (error) {
+			toast({
+				title: "Failed to send message.",
+				description: "Please try again later, or contact me using my email..",
+				status: "error",
+				variant: "left-accent",
+				position: "bottom-right",
+				duration: 3000,
+			});
+			console.error("Failed to send email: ", error);
+		} finally {
+			setSubmitting(false);
+			setLoading(false);
+		}
+	};
 
-    return(
-        <Container 
-        id="Contact"
-        h="auto"
-        minH="100vh"
-        px={0}
-        pt={{
-          base: 10,
-          md: 24
-        }}
-        pb={18}
-        display="flex"
-        flexDirection="column"
-        justifyContent="space-between"
-        alignItems="start"
-        gap={16}
-        className="w-[375px] sm:w-[480px] md:w-[674px] lg:w-[894px] xl:w-full min-w-[375px] sm:min-w-[480px] md:min-w-[674px] lg:min-w-[894px] xl:min-w-full"
-        >
-          
-            <Flex 
-            direction={{
-                base:"column",
-                xl: "row"
-            }}
-            justify="start"
-            align={{
-                base: "center",
-                xl: "start" 
-            }}
-            w="100%"
-            gap={16}
-            >
-                <Container display="flex" flexDirection="column" justifyContent="start" alignItems="start" gap={8} w="fit-content" m={0}>
-                    <Text className="text-3xl font-bold w-full">
-                        Do you have a <span className="text-[#3182CE]">question</span> to ask or an <span className="text-[#3182CE]">opportunity</span> to present?
-                    </Text>
-                    <Text className="text-4xl font-bold w-full">
-                        Feel free to <span className="text-[#3182CE]">reach out</span>.
-                    </Text>
-                </Container>
-                <Formik
-                initialValues={{ name: "", email: "", subject: "", message: "" }}
-                validationSchema={toFormikValidationSchema(validationSchema)}
-                onSubmit={handleSubmit}
-                >
-                    <Form className="w-full flex justify-center items-center">
-                        <Grid
-                        templateColumns={"1fr 1fr"}
-                        gap={6} 
-                        flexGrow={1}
-                        w="100%"
-                        maxW="720px"
-                        px={4}
-                        >
-                            <GridItem colSpan={{ base: 2, md: 1 }}>
-                                <FormControl isRequired>
-                                    <FormLabel>Your Name</FormLabel>
-                                    <Field id="name" name="name" type="text" placeholder="John Doe" className="py-2 px-4 bg-transparent rounded-md border focus:shadow-[0_0_0_2px_#60a5fa] focus:shadow-blue-400 focus:outline-none transition-shadow duration-300 w-full" />
-                                    <ErrorMessage name="name" component="div" className="text-red-500" />
-                                </FormControl>
-                            </GridItem>
+	return (
+		<Container
+			id="Contact"
+			h="auto"
+			minH="100vh"
+			px={0}
+			pt={{
+				base: 10,
+				md: 24,
+			}}
+			pb={18}
+			display="flex"
+			flexDirection="column"
+			justifyContent="space-between"
+			alignItems="start"
+			gap={16}
+			className="w-[375px] sm:w-[480px] md:w-[674px] lg:w-[894px] xl:w-full min-w-[375px] sm:min-w-[480px] md:min-w-[674px] lg:min-w-[894px] xl:min-w-full"
+		>
+			<Flex
+				direction={{
+					base: "column",
+					xl: "row",
+				}}
+				justify="start"
+				align={{
+					base: "center",
+					xl: "start",
+				}}
+				w="100%"
+				gap={16}
+			>
+				<Container
+					display="flex"
+					flexDirection="column"
+					justifyContent="start"
+					alignItems="start"
+					gap={8}
+					w="fit-content"
+					m={0}
+				>
+					<Text className="text-3xl font-bold w-full">
+						Do you have a <span className="text-[#3182CE]">question</span> to
+						ask or an <span className="text-[#3182CE]">opportunity</span> to
+						present?
+					</Text>
+					<Text className="text-4xl font-bold w-full">
+						Feel free to <span className="text-[#3182CE]">reach out</span>.
+					</Text>
+				</Container>
+				<Formik
+					initialValues={{ name: "", email: "", subject: "", message: "" }}
+					validationSchema={toFormikValidationSchema(validationSchema)}
+					onSubmit={handleSubmit}
+				>
+					<Form className="w-full flex justify-center items-center">
+						<Grid
+							templateColumns={"1fr 1fr"}
+							gap={6}
+							flexGrow={1}
+							w="100%"
+							maxW="720px"
+							px={4}
+						>
+							<GridItem colSpan={{ base: 2, md: 1 }}>
+								<FormControl isRequired>
+									<FormLabel>Your Name</FormLabel>
+									<Field
+										id="name"
+										name="name"
+										type="text"
+										placeholder="John Doe"
+										className="py-2 px-4 bg-transparent rounded-md border focus:shadow-[0_0_0_2px_#60a5fa] focus:shadow-blue-400 focus:outline-none transition-shadow duration-300 w-full"
+									/>
+									<ErrorMessage
+										name="name"
+										component="div"
+										className="text-red-500"
+									/>
+								</FormControl>
+							</GridItem>
 
-                            <GridItem colSpan={{ base: 2, md: 1 }}>
-                                <FormControl>
-                                    <FormLabel>Your Email</FormLabel>
-                                    <Field id="email" name="email" type="email" placeholder="john@mail.com" className="py-2 px-4 bg-transparent rounded-md border focus:shadow-[0_0_0_2px_#60a5fa] focus:shadow-blue-400 focus:outline-none transition-shadow duration-300 w-full" />
-                                    <ErrorMessage name="email" component="div" className="text-red-500" />
-                                </FormControl>
-                            </GridItem>
+							<GridItem colSpan={{ base: 2, md: 1 }}>
+								<FormControl>
+									<FormLabel>Your Email</FormLabel>
+									<Field
+										id="email"
+										name="email"
+										type="email"
+										placeholder="john@mail.com"
+										className="py-2 px-4 bg-transparent rounded-md border focus:shadow-[0_0_0_2px_#60a5fa] focus:shadow-blue-400 focus:outline-none transition-shadow duration-300 w-full"
+									/>
+									<ErrorMessage
+										name="email"
+										component="div"
+										className="text-red-500"
+									/>
+								</FormControl>
+							</GridItem>
 
-                            <GridItem colSpan={2}>
-                                <FormControl>
-                                    <FormLabel>Subject</FormLabel>
-                                    <Field id="subject" name="subject" type="text" placeholder="Subject..." className="py-2 px-4 bg-transparent rounded-md border focus:shadow-[0_0_0_2px_#60a5fa] focus:shadow-blue-400 focus:outline-none transition-shadow duration-300 w-full" />
-                                    <ErrorMessage name="subject" component="div" className="text-red-500" />
-                                </FormControl>
-                            </GridItem>
+							<GridItem colSpan={2}>
+								<FormControl>
+									<FormLabel>Subject</FormLabel>
+									<Field
+										id="subject"
+										name="subject"
+										type="text"
+										placeholder="Subject..."
+										className="py-2 px-4 bg-transparent rounded-md border focus:shadow-[0_0_0_2px_#60a5fa] focus:shadow-blue-400 focus:outline-none transition-shadow duration-300 w-full"
+									/>
+									<ErrorMessage
+										name="subject"
+										component="div"
+										className="text-red-500"
+									/>
+								</FormControl>
+							</GridItem>
 
-                            <GridItem colSpan={2}>
-                                <FormControl isRequired>
-                                    <FormLabel>Your Message</FormLabel>
-                                    <Field as="textarea" id="message" name="message" placeholder="Message..." className="py-2 px-4 bg-transparent rounded-md border focus:shadow-[0_0_0_2px_#60a5fa] focus:shadow-blue-400 focus:outline-none transition-shadow duration-300 w-full min-h-32 resize-none" />
-                                    <ErrorMessage name="message" component="div" className="text-red-500" />
-                                </FormControl>
-                            </GridItem>
-                        
-                            <GridItem colSpan={{ base: 2, xl: 1 }}>
-                                <Button 
-                                type="submit"
-                                colorScheme="blue" 
-                                size="md" 
-                                w={{base: "full", xl:"fit-content"}} 
-                                isLoading={isLoading}
-                                >
-                                    Send Message
-                                </Button>
-                            </GridItem>
-                        </Grid>
-                    </Form>
-                </Formik>
-            </Flex>
+							<GridItem colSpan={2}>
+								<FormControl isRequired>
+									<FormLabel>Your Message</FormLabel>
+									<Field
+										as="textarea"
+										id="message"
+										name="message"
+										placeholder="Message..."
+										className="py-2 px-4 bg-transparent rounded-md border focus:shadow-[0_0_0_2px_#60a5fa] focus:shadow-blue-400 focus:outline-none transition-shadow duration-300 w-full min-h-32 resize-none"
+									/>
+									<ErrorMessage
+										name="message"
+										component="div"
+										className="text-red-500"
+									/>
+								</FormControl>
+							</GridItem>
 
-            <Socials />
-        </Container>
-    )
+							<GridItem colSpan={{ base: 2, xl: 1 }}>
+								<Button
+									type="submit"
+									colorScheme="blue"
+									size="md"
+									w={{ base: "full", xl: "fit-content" }}
+									isLoading={isLoading}
+								>
+									Send Message
+								</Button>
+							</GridItem>
+						</Grid>
+					</Form>
+				</Formik>
+			</Flex>
+
+			<Socials />
+		</Container>
+	);
 }
--- v4.0/components/Project.tsx
+++ v4.0/components/Project.tsx
@@ -1,93 +1,139 @@
-'use client'
+"use client";
 
 import { DirectionAwareHover } from "./HoverCard";
 import Carousel from "./Carousel";
-import { ModalOverlay, Button, Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Divider, useDisclosure, Flex, Container, Text } from "@chakra-ui/react";
+import {
+	ModalOverlay,
+	Button,
+	Modal,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	Divider,
+	useDisclosure,
+	Flex,
+	Container,
+	Text,
+} from "@chakra-ui/react";
 import { Image, Link } from "@chakra-ui/next-js";
-import { FaGithub } from "react-icons/fa";
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
 export default function Project({
-    content,
+	content,
 }: {
-    content: {
-        title: string;
-        description: string;
-        stack: string;
-        images: string[];
-        linkUrl?: string;
-    };
+	content: {
+		title: string;
+		description: string;
+		stack: string;
+		images: string[];
+		linkUrl?: string;
+		githubUrl?: string;
+	};
 }) {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    
-    return (
-        <>
-            <Button
-            h="fit-content"
-            w="fit-content"
-            px={0}
-            onClick={() => {
-                onOpen()
-            }}
-            >
-                <DirectionAwareHover imageUrl={content.images[0]}>
-                    <p className="font-bold text-xl">{content.title}</p>
-                </DirectionAwareHover>
-            </Button>
-            
-            <Modal isCentered isOpen={isOpen} onClose={onClose} size="6xl">
-                <ModalOverlay bg='blackAlpha.700' backdropFilter='blur(10px)'/>
-                <ModalContent 
-                bg="primary" 
-                p={{
-                base: 2,
-                md: 8
-                }}
-                gap={{
-                    base: 2,
-                    md: 4
-                }}
-                >
-                    <ModalHeader 
-                    fontSize={{
-                        base: 16,
-                        sm:24
-                    }}
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    >
-                        {content.title} 
-                        {content.linkUrl && 
-                        <Link href={content.linkUrl} isExternal className="inline-block align-middle ml-4">
-                            <FaGithub size={32} className="hover:text-blue-500 cursor-pointer transition-all"/>
-                        </Link>}
-                    </ModalHeader>
-                    <ModalCloseButton />
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
-                    <ModalBody>
-                        <Flex
-                        direction="column"
-                        justify="start"
-                        align="center"
-                        gap={{
-                            base: 0,
-                            md: 2
-                        }}
-                        >
-                            <Container minW="full" display="flex" flexDir="column" gap={4}>
-                                <Text w="w-full" className="text-justify whitespace-pre-wrap">{content.description}</Text>
-                                <Divider className="bg-white"/>
-                                <Text w="full" className="text-gray-400 text-justify">{content.stack}</Text>
-                            </Container>
-                            <Carousel autoSlide={true}>
-                                {content.images.map((image, index) => (
-                                    <Image key={index} src={image} alt={content.title} width={720} height={405} className="inset-0 min-w-full object-contain" unoptimized={true} />
-                                ))}
-                            </Carousel>
-                        </Flex>
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        </>
-    )
+	return (
+		<>
+			<Button
+				h="fit-content"
+				w="fit-content"
+				px={0}
+				onClick={() => {
+					onOpen();
+				}}
+			>
+				<DirectionAwareHover imageUrl={content.images[0]}>
+					<p className="font-bold text-xl">{content.title}</p>
+				</DirectionAwareHover>
+			</Button>
+
+			<Modal isCentered isOpen={isOpen} onClose={onClose} size="6xl">
+				<ModalOverlay bg="blackAlpha.700" backdropFilter="blur(10px)" />
+				<ModalContent
+					bg="primary"
+					p={{
+						base: 2,
+						md: 8,
+					}}
+					gap={{
+						base: 2,
+						md: 4,
+					}}
+				>
+					<ModalHeader
+						fontSize={{
+							base: 16,
+							sm: 24,
+						}}
+						display="flex"
+						justifyContent="space-between"
+						alignItems="center"
+					>
+						{content.title}
+						{content.linkUrl && (
+							<Link
+								href={content.linkUrl}
+								isExternal
+								className="inline-block align-middle ml-4 mt-1"
+							>
+								<FaExternalLinkAlt
+									size={24}
+									className="hover:text-blue-500 cursor-pointer transition-all"
+								/>
+							</Link>
+						)}
+						{content.githubUrl && (
+							<Link
+								href={content.githubUrl}
+								isExternal
+								className="inline-block align-middle ml-4"
+							>
+								<FaGithub
+									size={32}
+									className="hover:text-blue-500 cursor-pointer transition-all"
+								/>
+							</Link>
+						)}
+					</ModalHeader>
+					<ModalCloseButton />
+
+					<ModalBody>
+						<Flex
+							direction="column"
+							justify="start"
+							align="center"
+							gap={{
+								base: 0,
+								md: 2,
+							}}
+						>
+							<Container minW="full" display="flex" flexDir="column" gap={4}>
+								<Text w="w-full" className="whitespace-pre-wrap">
+									{content.description}
+								</Text>
+								<Divider className="bg-white" />
+								<Text w="full" className="text-gray-400 text-justify">
+									{content.stack}
+								</Text>
+							</Container>
+							<Carousel autoSlide={true}>
+								{content.images.map((image, index) => (
+									<Image
+										key={index}
+										src={image}
+										alt={content.title}
+										width={720}
+										height={405}
+										className="inset-0 min-w-full object-contain"
+										unoptimized={true}
+									/>
+								))}
+							</Carousel>
+						</Flex>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+		</>
+	);
 }
